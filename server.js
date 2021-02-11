533916101642f1f381d847220e91f5f7ec717d4d
Torontow <mtorontow0@gmail.com>
2021-02-10 20:19:33 -0500

    Added html routes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,11 +17,37 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // may not have to do this here because it is in the seed.js
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // INSERT CODE HERE
 
+// get index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// get exercise.html
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+// get stats.html
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/stats.html"))
+});
 // GET last workout "/api/workouts" (find method)
+
+// app.get("/workouts", (req, res) => {
+//   db.Note.find({})
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+
 
 // ADD an exercise "/api/workouts/:id" (put method)
 
